2f3e7c83582ddaa3449bda1d8d1a2b56fdc01839
WilsonHua <huazhenghao@le.com>
2017-01-16 17:23:55 +0800

    webpack config

--- config/index.js
+++ config/index.js
@@ -9,8 +9,6 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: false,
@@ -25,8 +23,6 @@ module.exports = {
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
     cssSourceMap: false
   }
 }
